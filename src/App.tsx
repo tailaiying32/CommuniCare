71d09b36dd87f1b63578e428f4c77e43bb02b4c3
TailaiYing <taiying9627@gmail.com>
2025-01-25 14:15:22 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+// why is this not working
+function App() {
+  return (
+    <>
+      <p className="bg-blue-100 text-6xl font-bold underline underline-offset-auto">
+        This is CommuniCare
+      </p>
+    </>
+  );
+}
+
+export default App;
