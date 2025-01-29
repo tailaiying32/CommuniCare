d595006ce95b3ef8512b644fc0c1320f7f4a8a31
TailaiYing <taiying9627@gmail.com>
2025-01-29 15:43:35 -0500

    changed structure of project - whole pages are now in the pages folder, and components in the components folder. Renamed the main app.tsx to home.tsx to indicate the home page and to accomodate the actual meet the app page, now titled app.tsx

---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import NavBar from "../components/Navbar";
+import "./Home.css";
+
+// testing git
+
+function Home() {
+  return (
+    <>
+      <NavBar />
+      <p className="text-6xl font-bold text-neutral-900 underline underline-offset-auto">
+        This is CommuniCare
+      </p>
+    </>
+  );
+}
+
+export default Home;
