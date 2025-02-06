d634a76ec3cc8fc81667a8b8ae6c940ac6e3d4ed
Oreoluwa <oaa42@cornell.edu>
2025-02-06 18:04:21 -0500

    Currently working on Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,14 +2,18 @@ import NavBar from "../components/Navbar";
 import "./Home.css";
 
 // testing git
-//ygygygdiqwhxuwxhwgxwtuhwx qwt
+
 function Home() {
   return (
     <>
       <NavBar />
-      <p className="text-6xl font-bold text-neutral-900 underline underline-offset-auto">
-        This is CommuniCare
+      <p className="text-6xl font-bold text-neutral-800">
+      Find the Right Healthcare Whenever You Need. 
       </p>
+      <p className="text-2xl font-normal text-neutral-800">
+      CommuniCare will help connect you with free healthcare services nearby because you deserve to feel better. 
+      </p>
+    
     </>
   );
 }
